f8a9f01f16994a6a66b2f5df1d060f7ead1cda0b
tiger <44923556+eeeeaa@users.noreply.github.com>
2024-06-13 19:25:04 +0700

    implement comment route test

--- test/commentsTest.test.js
+++ test/commentsTest.test.js
@@ -61,15 +61,71 @@ describe("comment route test", () => {
         expect(res.body.comment.author).toBe(userId);
         expect(res.body.comment.post).toBe(postId);
         expect(res.body.comment.message).toBe("test message");
+        expect(res.body.comment.like_count).toBe(0);
         commentId = res.body.comment._id;
         return done();
       });
   });
 
-  /*test("get comments", (done) => {});
-  test("get one comment", (done) => {});
+  test("get comments", (done) => {
+    request(app)
+      .get(`/${postId}/comments`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.comments.length).toBe(1);
+        expect(res.body.comments[0].author).toBe(userId);
+        expect(res.body.comments[0].post).toBe(postId);
+        return done();
+      });
+  });
+  test("get one comment", (done) => {
+    request(app)
+      .get(`/${postId}/comments/${commentId}`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.comment.author).toBe(userId);
+        expect(res.body.comment.post).toBe(postId);
+        expect(res.body.comment.message).toBe("test message");
+        expect(res.body.comment.like_count).toBe(0);
+        return done();
+      });
+  });
 
-  test("update comment", (done) => {});
+  test("update comment", (done) => {
+    request(app)
+      .put(`/${postId}/comments/${commentId}`)
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        message: "test message updated",
+        like_count: 5,
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.updatedComment.author).toBe(userId);
+        expect(res.body.updatedComment.post).toBe(postId);
+        expect(res.body.updatedComment.message).toBe("test message updated");
+        expect(res.body.updatedComment.like_count).toBe(5);
+        return done();
+      });
+  });
 
-  test("delete comment", (done) => {});*/
+  test("delete comment", (done) => {
+    request(app)
+      .delete(`/${postId}/comments/${commentId}`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.deletedComment.author).toBe(userId);
+        expect(res.body.deletedComment.post).toBe(postId);
+        expect(res.body.deletedComment.message).toBe("test message updated");
+        expect(res.body.deletedComment.like_count).toBe(5);
+        return done();
+      });
+  });
 });
